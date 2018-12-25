abc369a0997f95b5b0ff0aac567f641a84725e3f
LNWDARK <hnam@nextgensoft.co.th>
2018-12-25 17:16:58 +0700

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-echo "install"
-sudo apt update && upgrade
-sudo apt-get install fonts-thai-tlwg && unclutter && php 
+echo "check update"
+sudo apt update 
+sleep 2s
+echo 'check upgrade'
+sudo apt upgrade
+sleep 2s
+echo 'install fonts-thai'
+sudo apt-get install fonts-thai-tlwg
+sleep 2s
+echo 'install unclutter'
+sudo apt-get install unclutter
+sleep 2s
+echo 'install php'
+sudo apt-get install php
