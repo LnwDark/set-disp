e4e65c293dc750c726eca0d3475f6635613640dd
LNWDARK <hnam@nextgensoft.co.th>
2019-01-05 01:38:12 +0700

    edit cate

--- kiosk.sh
+++ kiosk.sh
@@ -4,8 +4,8 @@ export DISPLAY=:0
 # Hide the mouse from the display
 unclutter &
 # If Chrome crashes (usually due to rebooting), clear the crash flag so we don't have the annoying warning bar
-sed -i 's/"exited_cleanly":false/"exited_cleanly":false/' ~/.config/chromium/'Local State'
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
+sed -i 's/"exited_cleanly":false/"exited_cleanly":false/' /home/monitor/.config/chromium/'Local State'
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' /home/monitor/.config/chromium/Default/Preferences
 # Run Chromium and open tabs
 link_url=https://bo.nextschool.io/monitor/messaging
 config=
