77610b793a0b3c2d6e49d0d243f28b320678ed23
LNWDARK <hnam@nextgensoft.co.th>
2019-01-07 15:45:54 +0700

    edit kiosk

--- kiosk.sh
+++ kiosk.sh
@@ -4,11 +4,18 @@ export DISPLAY=:0
 # Hide the mouse from the display
 unclutter &
 # If Chrome crashes (usually due to rebooting), clear the crash flag so we don't have the annoying warning bar
-sed -i 's/"exited_cleanly":false/"exited_cleanly":false/' ~/.config/chromium/'Local State'
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
+sed -i 's/"exited_cleanly":false/"exited_cleanly":false/' /home/monitor/.config/chromium/'Local State'
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' /home/monitor/.config/chromium/Default/Preferences
 # Run Chromium and open tabs
 link_url=https://bo.nextschool.io/monitor/terminal
 while true;
-do ping -c1 www.google.com > /dev/null && echo 'online' && /usr/bin/chromium-browser --disable-translate --noerrordialogs --kiosk --disable-session-crashed-bubble $link_url
+do ping -c1 www.google.com > /dev/null && echo 'online' && /usr/bin/chromium-browser \
+--disable-translate \
+--noerrordialogs \
+--kiosk \
+--disable-glsl-translator \
+--disable-session-crashed-bubble \
+--disable-infobars \
+$link_url
 sleep 2s
 done
