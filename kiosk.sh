af9e047bd3b28f3b966430b2d2a5c9d344e612a2
LNWDARK <hnam@nextgensoft.co.th>
2019-01-08 11:16:11 +0700

    kiosk

--- kiosk.sh
+++ kiosk.sh
@@ -6,17 +6,20 @@ unclutter &
 # If Chrome crashes (usually due to rebooting), clear the crash flag so we don't have the annoying warning bar
 sed -i 's/"exited_cleanly":false/"exited_cleanly":false/' ~/.config/chromium/'Local State'
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
+
+sed -i 's/"exited_cleanly":false/"exited_cleanly":false/' /home/monitor/.config/chromium/'Local State'
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' /home/monitor/.config/chromium/Default/Preferences
 # Run Chromium and open tabs
 link_url=https://bo.nextschool.io/monitor/terminal
 while true;
 do ping -c1 www.google.com > /dev/null && echo 'online' && /usr/bin/chromium-browser \
+--kiosk \
 --noerrors \
 --disable-translate \
 --noerrordialogs \
 --disable-glsl-translator \
 --disable-session-crashed-bubble \
 --disable-infobars \
---kiosk \
 $link_url
 sleep 2s
 done
